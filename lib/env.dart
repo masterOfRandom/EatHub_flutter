04140366acaaa8961cc4867b5c396300dc419d8e
minje70 <minje70@naver.com>
2022-05-23 16:30:39 +0900

    [hotfix]

---
+++ lib/env.dart
@@ -0,0 +1,10 @@
+const KAKAO_REST_API_KEY = 'd4c4f8d6994c73117a8f2b098aed05c6';
+const KAKAO_CATEGORY_GROUP_CODE = {
+  '음식점': 'FD6',
+  '카페': 'CE7',
+  '대형마트': 'MT1',
+  '편의점': 'CS2',
+};
+
+const ANDROIDID = 'com.mijeong.table_pick';
+const IOSID = 'com.masterOfRandom.EatHub';
